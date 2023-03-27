2239977a9172865c6b5a03d69adc5efd9749d885
Andrey Acosta <andreyacost24ra18@gmail.com>
2023-03-27 13:43:05 -0600

    update Paragraphs.js

--- Web-UI/Scripts/Views/Paragraphs.js
+++ Web-UI/Scripts/Views/Paragraphs.js
@@ -1,81 +1,67 @@
 ﻿class Paragraphs {
+    constructor() {
+        this.paragraphData = null;
+    }
+
     LoadParagraphsTable() {
         const arrayColumns = [
             { data: "wordsStartingWithMyName" },
         ];
 
         $("#tblParagraphs").DataTable({
-            ajax: {
-                method: "GET",
-                url: "https://acost-wiki-reader-lab1.azurewebsites.net/api/Text/GetAllText",
-                contentType: "application/json;charset=utf-8",
-                dataSrc: (json) => {
-                    const paragraphData = {
-                        wordsStartingWithMyName: json.wordsStartingWithMyName.join(", ")
-                    };
-                    return [paragraphData];
-                },
-            },
+            data: [this.paragraphData],
             columns: [
                 { data: "wordsStartingWithMyName" }
             ],
         });
 
-        this.LoandAverageWordsPerParagraph();
-
+        this.LoadAverageWordsPerParagraph();
+        this.LoadParagraphWithMostWords();
+        this.LoadParagraphWithFewestWords();
     };
 
-    LoandAverageWordsPerParagraph() {
-        $.ajax({
-            url: "https://acost-wiki-reader-lab1.azurewebsites.net/api/Text/GetAllText",
-            type: "GET",
-            dataType: "json",
-            success: function (result) {
-                const averageWordsPerParagraph = result.averageWordsPerParagraph.toFixed(2);
-                $("#txtpromedio").val(averageWordsPerParagraph);
-            },
-            error: function (xhr, status, error) {
-                console.log("Error al obtener los datos");
-            },
+    LoadDataFromAPI() {
+        return new Promise((resolve, reject) => {
+            $.ajax({
+                type: "GET",
+                url: "https://acost-wiki-reader-lab1.azurewebsites.net/api/Text/GetAllText",
+                contentType: "application/json;charset=utf-8",
+                success: function (response) {
+                    resolve(response);
+                },
+                error: function (xhr, status, error) {
+                    reject("Error al obtener los datos");
+                },
+            });
         });
-
-        this.LoandParagraphWithMostWords();
     }
 
-    LoandParagraphWithMostWords() {
-        $.ajax({
-            type: "GET",
-            url: "https://acost-wiki-reader-lab1.azurewebsites.net/api/Text/GetAllText",
-            contentType: "application/json;charset=utf-8",
-            success: function (response) {
-                const paragraphWithMostWords = response.paragraphWithMostWords.paragraphValue;
-                $("#txtMas").val(paragraphWithMostWords);
-            },
-            error: function (xhr, status, error) {
-                console.log("Error al obtener los datos");
-            },
-        });
-
-        this.paragraphWithFewestWords();
+    async LoadParagraphData() {
+        try {
+            this.paragraphData = await this.LoadDataFromAPI();
+            this.LoadParagraphsTable();
+        } catch (error) {
+            console.log(error);
+        }
     }
 
-    paragraphWithFewestWords() {
-        $.ajax({
-            type: "GET",
-            url: "https://acost-wiki-reader-lab1.azurewebsites.net/api/Text/GetAllText",
-            contentType: "application/json;charset=utf-8",
-            success: function (response) {
-                const paragraphWithFewestWords = response.paragraphWithFewestWords.paragraphValue;
-                $("#txtMenos").val(paragraphWithFewestWords);
-            },
-            error: function (xhr, status, error) {
-                console.log("Error al obtener los datos");
-            },
-        });
+    LoadAverageWordsPerParagraph() {
+        const averageWordsPerParagraph = this.paragraphData.averageWordsPerParagraph.toFixed(2);
+        $("#txtpromedio").val(averageWordsPerParagraph);
+    }
+
+    LoadParagraphWithMostWords() {
+        const paragraphWithMostWords = this.paragraphData.paragraphWithMostWords.paragraphValue;
+        $("#txtMas").val(paragraphWithMostWords);
+    }
+
+    LoadParagraphWithFewestWords() {
+        const paragraphWithFewestWords = this.paragraphData.paragraphWithFewestWords.paragraphValue;
+        $("#txtMenos").val(paragraphWithFewestWords);
     }
 }
 
 $(document).ready(() => {
     const view = new Paragraphs();
-    view.LoadParagraphsTable();
+    view.LoadParagraphData();
 });
